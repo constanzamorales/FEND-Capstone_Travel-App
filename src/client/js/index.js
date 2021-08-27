1f8ed451cb4b0ccfe40c4cbb57eddb13cbe170d5
constanzamorales <constanzabmorales@gmail.com>
2021-08-27 15:05:36 -0400

    Set up the server

---
+++ src/client/js/index.js
@@ -0,0 +1,32 @@
+var path = require('path');
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const fetch = require('node-fetch');
+
+const app = express();
+
+dotenv.config();
+// const apiKey = process.env.API_KEY;
+
+app.use(cors());
+
+// Middleware
+app.user(express.urlencoded({ extended: false }));
+app.user(express.json());
+
+app.use(express.static('dist'));
+
+const port = 8081;
+const server = app.listen(port, () => {
+    console.log(`Running on ${port} :D`);
+});
+
+app.get('/', function (request, response) {
+    response.sendFile(path.resolve('dist/index.html'))
+});
+
+app.post('/addData', addData);
+async function addData(request, response) {
+    const res = await fetch(`https://`)
+}
