424f802fbd7e29742ede82dc33a5372e3f4616a3
constanzamorales <constanzabmorales@gmail.com>
2021-08-31 10:45:52 -0400

    Change name of route to /appData

--- src/client/js/getData.js
+++ src/client/js/getData.js
@@ -2,7 +2,7 @@ function getCity(event) {
     event.preventDefault();
 
     let userInput = document.getElementById('city').value;
-    fetch('http://localhost:8081/weatherData', {
+    fetch('http://localhost:8081/appData', {
         method: 'POST',
         credentials: 'same-origin',
         headers: {
@@ -13,11 +13,11 @@ function getCity(event) {
     .then(res => res.json())
     .then(function(res) {
         console.log(res);
-        // const lat = res.data[0].lat;
-        // const long = res.data[0].lon;
-        // Here goes the function call to convert lat and lon to city
-        // convertLocation(res.data[0].lat, res.data[0].lon);
-        document.getElementById('results').textContent = res.data[0].app_temp;
+        /*
+        const lat = res.geonames[0].lat;
+        const lon = res.geonames[0].lng;
+        console.log(lat, lon);
+        */
     })
 }
 
