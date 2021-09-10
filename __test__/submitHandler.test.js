85af99003173ddc5a4c404d3b593607b5a0de33f
constanzamorales <constanzabmorales@gmail.com>
2021-09-10 11:52:42 -0300

    Fix service workers

--- __test__/submitHandler.test.js
+++ __test__/submitHandler.test.js
@@ -1,7 +1,5 @@
 import { handleSubmit } from "../src/client/js/submitHandler";
 
-// The describe() function takes two arguments - a string description, and a test suite as a callback function.  
-// A test suite may contain one or more related tests    
 describe("Testing the submit functionality", () => {
     test("Testing the handleSubmit() function", () => {
         expect(handleSubmit).toBeDefined();
--- webpack.prod.js
+++ webpack.prod.js
@@ -4,7 +4,7 @@ const HtmlWebPackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin')
-// const WorkboxPlugin = require('workbox-webpack-plugin')
+const WorkboxPlugin = require('workbox-webpack-plugin')
 
 module.exports = {
     entry: './src/client/index.js',
@@ -52,6 +52,6 @@ module.exports = {
             { 
             filename: '[name].css'
         }),
-        // new WorkboxPlugin.GenerateSW()
+        new WorkboxPlugin.GenerateSW()
     ]
 }
